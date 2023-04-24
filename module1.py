1d9fbfa7cf424ec7aa5e6e49a40e53408e59eb68
Austin Snodgrass <71675352+alsnodgrass@users.noreply.github.com>
2023-04-24 14:02:05 -0400

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,30 @@
+"""
+Author: Austin Snodgrass
+Name: OPS+ Calcualator
+Purpose: Take user input and use it to calculate players OBP, Slugging and OPS+
+"""
+
+#Retrieving user input to calculate player statistics
+player = input('Player: ')
+hits = float(input('Hits: '))
+doubles = float(input('Doubles: '))
+triples = float(input('Triples: '))
+homeRuns = float(input('Home Runs: '))
+singles = hits - doubles - triples - homeRuns
+walks = float(input('Walks: '))
+hbp = float(input('Hit by Pitch: '))
+sacFlies = float(input('Sacrifice Flies: '))
+atBats = float(input('At Bats: '))
+
+#Setting league statistic variables
+leagueOnBase = 0.320
+leagueSlugging = 0.402
+
+#Using user input to calculate player statistics and format values to be printed
+onBase = round((hits + walks + hbp) / (atBats + walks + hbp + sacFlies), 3)
+slugging = round((singles + (doubles * 2) + (triples *3) + (homeRuns * 4)) / atBats, 3)
+
+opsPlus = int(100 * ((onBase / leagueOnBase) + (slugging / leagueSlugging) - 1))
+
+#Printing calculated player statistics
+print(player + ' has an OBP of ' + str(onBase) + ' with a slugging of ' + str(slugging) + ' for an OPS+ of ' + str(opsPlus))
